474b3835570d6e5d764ec4e3291989ce1867e890
Balesh <baleshvarshney078@gmail.com>
2025-09-02 13:42:46 +0530

    2 file modified

--- Netflix/src/Components/SeriesCard.jsx
+++ Netflix/src/Components/SeriesCard.jsx
@@ -1,8 +1,14 @@
 const SeriesCard = ({Element}) =>{
    const {img_url , name , rating , description , cast , watch_url , id} = Element;
 
+   const btn_style = {
+              padding:"1.2rem 2.4rem",
+              fontSize:"1.4rem",
+              backgroundColor:"#e50914",
+            }
+
      return( 
-     <li >
+     <li className="card" >
           <div>
             <img
               src={img_url}
@@ -11,13 +17,15 @@ const SeriesCard = ({Element}) =>{
               height="40%"
             />
           </div>
+          <div className="card-content">
           <h2>Name : {name}</h2>
           <h3>Rating : {rating}</h3>
           <p>Summary : {description}</p>
           <p>Cast : {cast}</p>
           <a href={watch_url} target="_blank">
-            <button>Watch Now</button>
+            <button style={btn_style}>Watch Now</button>
           </a>
+          </div>
         </li>);
 }
 
